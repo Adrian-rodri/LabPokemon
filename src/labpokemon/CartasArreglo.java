25e8ad03f7ff797f86d733ea9657397f037d1e15
Debbido27 <davidmedina272004@gmail.com>
2026-02-06 11:01:38 -0600

    se conectaron los botones con las imagenes

--- src/labpokemon/CartasArreglo.java
+++ src/labpokemon/CartasArreglo.java
@@ -9,25 +9,24 @@ public class CartasArreglo {
     
     public static Carta[] crearCarta(){
         Carta[] cartas = new Carta[18];
-        cartas[0] = new CartaHija("Charmander");
-        cartas[1] = new CartaHija("Squirt");
-        cartas[2] = new CartaHija("Bulbasur");
-        cartas[3] = new CartaHija("Pikachu");
-        cartas[4] = new CartaHija("Jigglypuff");
-        cartas[5] = new CartaHija("Meowth");
-        cartas[6] = new CartaHija("Psyduck");
-        cartas[7] = new CartaHija("Growlithe");
-        cartas[8] = new CartaHija  ("Vulpix");
-        cartas[9]  = new CartaHija ("Magmar");
-        cartas[10] = new CartaHija ("Poliwag");
-        cartas[11] = new CartaHija ("Staryu");
-        cartas[12] = new CartaHija ("Seel");
-        cartas[13] = new CartaHija ("Lapras");
-        cartas[14] = new CartaHija ("Pidgey");
-        cartas[15] = new CartaHija ("Spearow");
-        cartas[16] = new CartaHija ("Zubat");
-        cartas[17] = new CartaHija("Hoothoot");        
-        
+        cartas[0] = new CartaHija("Charmander","/resources/charmande.png");
+        cartas[1] = new CartaHija("Squirtle", "/resources/squirtle.png");
+        cartas[2] = new CartaHija("Bulbasaur", "/resources/bulbasaur.png");
+        cartas[3] = new CartaHija("Pikachu", "/resources/pikachu.png");
+        cartas[4] = new CartaHija("Jigglypuff", "/resources/jigglypuff.png");
+        cartas[5] = new CartaHija("Meowth", "/resources/meowth.png");
+        cartas[6] = new CartaHija("Psyduck", "/resources/psyduck.png");
+        cartas[7] = new CartaHija("Growlithe", "/resources/growlithe.png");
+        cartas[8] = new CartaHija("Vulpix", "/resources/vulpix.png");
+        cartas[9] = new CartaHija("Magmar", "/resources/magmar.png");
+        cartas[10] = new CartaHija("Poliwag", "/resources/poliwag.png");
+        cartas[11] = new CartaHija("Staryu", "/resources/staryu.png");
+        cartas[12] = new CartaHija("Seel", "/resources/seel.png");
+        cartas[13] = new CartaHija("Lapras", "/resources/lapras.png");
+        cartas[14] = new CartaHija("Pidgey", "/resources/pidgey.png");
+        cartas[15] = new CartaHija("Spearow", "/resources/spearow.png");
+        cartas[16] = new CartaHija("Zubat", "/resources/zubat.png");
+        cartas[17] = new CartaHija("Hoothoot", "/resources/hoothoot.png");        
        return cartas;
     };
     
