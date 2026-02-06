9c8481efc29caaf24e47a6e2a5ffc377afd1a0ff
Debbido27 <davidmedina272004@gmail.com>
2026-02-06 10:29:32 -0600

    Arreglo pool terminado

--- src/labpokemon/CartasArreglo.java
+++ src/labpokemon/CartasArreglo.java
@@ -9,7 +9,26 @@ public class CartasArreglo {
     
     public static Carta[] crearCarta(){
         Carta[] cartas = new Carta[18];
+        cartas[0] = new CartaHija("Charmander");
+        cartas[1] = new CartaHija("Squirt");
+        cartas[2] = new CartaHija("Bulbasur");
+        cartas[3] = new CartaHija("Pikachu");
+        cartas[4] = new CartaHija("Jigglypuff");
+        cartas[5] = new CartaHija("Meowth");
+        cartas[6] = new CartaHija("Psyduck");
+        cartas[7] = new CartaHija("Growlithe");
+        cartas[8] = new CartaHija  ("Vulpix");
+        cartas[9]  = new CartaHija ("Magmar");
+        cartas[10] = new CartaHija ("Poliwag");
+        cartas[11] = new CartaHija ("Staryu");
+        cartas[12] = new CartaHija ("Seel");
+        cartas[13] = new CartaHija ("Lapras");
+        cartas[14] = new CartaHija ("Pidgey");
+        cartas[15] = new CartaHija ("Spearow");
+        cartas[16] = new CartaHija ("Zubat");
+        cartas[17] = new CartaHija("Hoothoot");        
         
+       return cartas;
     };
     
 }
